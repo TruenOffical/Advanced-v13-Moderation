0d16b1178a79c30454fbb4a73855ba5451a370e9
Truen <truenkings@gmail.com>
2022-03-09 19:37:15 +0300

    main

--- index.ts
+++ index.ts
@@ -19,6 +19,34 @@ client.on('messageCreate' , (message) => {
 
 client.on('ready', () => {
     console.log(`Developed By IX Media \n ${client.user?.tag} Adlı Bot Başarıyla Aktif Edildi`)
+    //Guild
+    const guildId = "949040954164252732";
+    const guild = client.guilds.cache.get(guildId);
+    let commands;
+
+    if(guild){
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+    commands?.create({
+        name:"ping",
+        description:"Pong olarak reply atar"
+    })
+    //Global
+})
+client.on("interactionCreate", async (interaction) => {
+    if(!interaction.isCommand()){
+        return;
+    }
+    const {commandName , options} = interaction;
+
+    if(commandName === "ping"){
+        interaction.reply({
+            content:'Pong',
+            ephemeral: true
+        })
+    }
 })
 
 client.login(process.env.TOKEN)
